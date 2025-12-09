faf5fa5545149bd83c70ee725442cfbb647668d9
sitrouye <best.elvire@gmail.com>
2025-12-09 19:28:57 +0000

    display detected spots with display code

--- combine.py
+++ combine.py
@@ -9,14 +9,17 @@ from morris_node_system import (
     detect_all_stones
 )
 from onnxtest import nms, load_model, preprocess
-
+import time
+from display import add_pieces, board
 
 ##Image and variables
 
-image_path = "metrics/images/IMG-20251207-WA0041.jpg"
+# image_path = "metrics/images/IMG-20251207-WA0041.jpg"
+image_path = "training/dataset/train/images/pieces12.jpg"
 rect_path = "metrics/images/rect41.jpg"
 res_path = "metrics/labels/rect41.txt"
-model = "models/best.onnx"
+# model = "models/best.onnx"
+model = "models/best1.onnx"
 CLASSES = ['board', 'whitefigure', 'blackfigure', 'emptyspot']
 INPUT_SIZE = 640  # same as training
 CONF_THRESHOLD = 0.3
@@ -24,6 +27,12 @@ IOU_THRESHOLD = 0.45
 
 class_names = ['Board', 'Whitefigure', 'Blackfigure', 'Emptyslot']
 
+radius = 1 #radius for circles at intersections
+p_radius = 2 #radius for the pieces
+origin = (0,0)
+step = (5,5)
+width = 50
+wstep = 10
 
 ## detecting board
 
@@ -113,16 +122,19 @@ def get_board(image_path):
     outer_rect, edges = detect_outer_paper_rect(frame)
     if outer_rect is None:
         print("Could not detect board outline.")
-        return 0,None, None
+        return 0
 
     # --- Rectify to square ---
     rect, H = rectify_to_square(frame, outer_rect)
     return rect
 
 def save_im(im, im_path):
-    cv2.imwrite(im_path, im)
-    k = cv2.waitKey(30)
-    cv2.destroyAllWindows()
+    try :
+        cv2.imwrite(im_path, im)
+        k = cv2.waitKey(30)
+        cv2.destroyAllWindows()
+    except:
+        print('could not save image')
     return im_path
 
 ##feeding board to CNN
@@ -154,7 +166,7 @@ def filter_nodes_class(res_path, im):
     and keeps the one with the highest score.
     '''
 
-    dim = np.shape(img)
+    dim = np.shape(im)
     file = open(res_path)
     lines = file.read()
     lines = lines.splitlines()
@@ -166,16 +178,6 @@ def filter_nodes_class(res_path, im):
         lab = []
         lab.append(int(parts[0]))
         x1, y1, x2, y2 = map(float, parts[1:5])
-
-        # x_c = (x2-x1)/2
-        # y_c = (y2-y1)/2
-        # w = x2-x1
-        # h = y2-y1
-
-        # lab.append(x_c)
-        # lab.append(y_c)
-        # lab.append(w)
-        # lab.append(h)
         lab.append(x1)
         lab.append(y1)
         lab.append(x2)
@@ -185,25 +187,26 @@ def filter_nodes_class(res_path, im):
     j =0
     indlist = [] #list of indices that were already compared
     while j < len(lablist):
-        if j not in indlist : 
-            lab = lablist[j]
-            x1, y1, x2, y2 = lab[1:5]
-            x_c_ref = (x2+x1)/(2)
-            y_c_ref = (y2+y1)/(2)
-            for i in range(len(lablist)) :
-                if i not in indlist :
-                    elt = lablist[i]
-                    x1, y1, x2, y2 = elt[1:5]
-                    x_c = (x2+x1)/(2)
-                    y_c = (y2+y1)/(2)
-                    #compare coordinates of centers
-                    if x_c_ref- 6 < x_c < x_c_ref + 6 : 
-                            if y_c_ref- 6 < y_c < y_c_ref + 6 :
-                                #centers are too close : one and only one label must remain
-                                indlist.append(i)
-                                if elt[5] > lab[5] : #keep line with the most confidence
-                                    lab = elt
-            new_lines.append(lab)
+        if lablist[j][0] != 0:
+            if j not in indlist : 
+                lab = lablist[j]
+                x1, y1, x2, y2 = lab[1:5]
+                x_c_ref = (x2+x1)/(2 * dim[1])
+                y_c_ref = (y2+y1)/(2 * dim[0])
+                for i in range(len(lablist)) :
+                    if i not in indlist :
+                        elt = lablist[i]
+                        x1, y1, x2, y2 = elt[1:5]
+                        x_c = (x2+x1)/(2 * dim[1])
+                        y_c = (y2+y1)/(2 * dim[0])
+                        #compare coordinates of centers
+                        if x_c_ref- 0.07< x_c < x_c_ref + 0.07 : 
+                                if y_c_ref- 0.07 < y_c < y_c_ref + 0.07 :
+                                    #centers are too close : one and only one label must remain
+                                    indlist.append(i)
+                                    if elt[5] > lab[5] : #keep line with the most confidence
+                                        lab = elt
+                new_lines.append(lab)
         j += 1
 
 
@@ -220,12 +223,53 @@ def filter_nodes_class(res_path, im):
     #     form_lines.append([elt[0], x1, y1, x2, y2, elt[5]])
     store_class(new_lines, res_path, filter = True)
 
+
+def sort(L, key):
+    L.sort(key=key)
+    return L
+
 def pieces_list(res_path):
     '''
     From the results txt file, 
     gets the list of white figures and black figures with numbered nodes
     these list can then be used as input in display.py
     '''
+    file = open(res_path, 'r')
+    lines = file.read()
+    lines = lines.splitlines()
+    #get the labels in the right order
+    lablist = []
+    new_lines = []
+    for i in range(len(lines)) : 
+        parts = lines[i].strip().split()
+        lab = []
+        lab.append(int(parts[0]))
+        x1, y1, x2, y2 = map(float, parts[1:5])
+        lab.append(x1)
+        lab.append(y1)
+        lab.append(x2)
+        lab.append(y2)
+        lab.append(float(parts[5]))
+        lablist.append(lab) 
+    #order them same as the corners
+    lablist.sort(key = lambda elt: (elt[2]+elt[4])/2 , reverse = False)
+    # lablist.sort(key = lambda elt: elt[1])
+    sortedlab = []
+    sortedlab = sort(lablist[0:3], key = lambda elt: (elt[1]+ elt[3])/2) + sort(lablist[3:6],  lambda elt: (elt[1]+ elt[3])/2) + sort(lablist[6:9],  lambda elt: (elt[1]+ elt[3])/2) +sort(lablist[9:15],  lambda elt: (elt[1]+ elt[3])/2) +sort(lablist[15:18],  lambda elt: (elt[1]+ elt[3])/2) +sort(lablist[18:21],  lambda elt: (elt[1]+ elt[3])/2) + sort(lablist[21:24],  lambda elt: (elt[1]+ elt[3])/2)
+    store_class(sortedlab,res_path, filter= True) 
+    #get list of pieces
+    w_pieces = []
+    b_pieces = []
+    empty= []
+    for i in range(len(sortedlab)):
+        if sortedlab[i][0] == 1 :
+            w_pieces.append(i)
+        elif sortedlab[i][0] == 2 :
+            b_pieces.append(i)
+        else : 
+            empty.append(i)
+    print(w_pieces, b_pieces, empty)
+    return w_pieces, b_pieces
 
 
 def draw_boxes_infer(img, results):
@@ -246,21 +290,24 @@ def draw_boxes_infer(img, results):
     return img
 
 
-def draw_boxes_filtered(image, labels):
-    img_h, img_w = image.shape[:2]
-
-    for label in labels:
-        x1, y1, x2, y2 = yolo_to_xyxy(label[1:5], img_w, img_h)
+def draw_boxes_filtered(img, results):
+    # color = [(0,255,0), (0,0,255), (255,0,0), (127,0,127)]
+    i = 0
+    for label in results:
+        
         class_name = class_names[int(label[0])]
         color = (70*int(label[0]),0,70*(3-int(label[0])))
+        x1,y1,x2,y2 = label[1:5]
+
         # Rectangle
-        cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
 
         # Label
-        cv2.putText(image, class_name, (x1, y1 - 5),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
-
-    return image
+        txlabel = class_name + ' ' + str(label[5])
+        cv2.putText(img, str(i), (int(x1), int(y1)-5), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.6, color, 2)
+        i += 1
+    return img
 
 
 def yolo_to_xyxy(box, img_w, img_h):
@@ -281,9 +328,13 @@ def yolo_to_xyxy(box, img_w, img_h):
 
 def test_yolo_label(image_path, label_path, class_names, resize, filtered = False):
     image = cv2.imread(image_path)
+    initsize = np.shape(image)
     if resize :
-        image = cv2.resize(image, None, fx = 0.4, fy = 0.4)
-
+        try :
+            image = cv2.resize(image, None, fx = 0.4, fy = 0.4)
+            newsize = np.shape(image)
+        except :
+            pass
     bboxes = []
     labels = []
 
@@ -292,6 +343,12 @@ def test_yolo_label(image_path, label_path, class_names, resize, filtered = Fals
             parts = line.strip().split()
             label = int(parts[0])
             x_c, y_c, w, h = map(float, parts[1:5])
+            if resize :
+                ratio = (newsize[0]/initsize[0], newsize[1]/initsize[1]) 
+                x_c = x_c * ratio[1]
+                y_c = y_c * ratio[0]
+                w = w * ratio[1]
+                h = h * ratio[0]
             bboxes.append([x_c, y_c, w, h])
             labels.append([label, x_c, y_c, w, h, float(parts[5])])
         
@@ -301,36 +358,64 @@ def test_yolo_label(image_path, label_path, class_names, resize, filtered = Fals
         result = draw_boxes_infer(image.copy(), labels)
     cv2.imshow("Label Test", result)
     cv2.waitKey(0)
+    return result
+
+
+
+def main(image_path, rect_path, res_path):
+    im = get_board(image_path)
+    im_path = save_im(im, rect_path)
+    img, lab = infer(model, rect_path)
+    store_class(lab, res_path)
+    
+    filter_nodes_class(res_path, img)
+    res_im = test_yolo_label(rect_path, res_path, class_names, resize = True, filtered=False)
+    save_im(res_im, rect_path)
+    w_pieces, b_pieces = pieces_list(res_path)
+    print(w_pieces, b_pieces)
+
+    axes, corner = board(radius, origin, step, width, wstep)
+    add_pieces(axes, corner, w_pieces, b_pieces)
+
+    
 
 ##display output
 
 if __name__ == "__main__" :
+    for i in range(56,68):
+        beg = time.time()
+        image_path = f"training/dataset/train/images/pieces{i}.jpg"
+        rect_path = f"metrics/NewModelResults/rect{i}.jpg"
+        res_path = f"metrics/NewModelResults/labels/rect{i}.txt"
+        main(image_path, rect_path, res_path)
+        # im = get_board(image_path)
+        # # im = cv2.resize(im, None, fx = 0.4, fy = 0.4)
+        # # title = f"rectified board"
+        # # cv2.namedWindow(title)
+        # # while True:
+        # #     cv2.imshow(title,im)
+        # #     k = cv2.waitKey(30)
+        # #     if k == 113:
+        # #         break
+        # # cv2.destroyAllWindows()
+        # im_path = save_im(im, rect_path)
+        # img, lab = infer(model, rect_path)
+        # # print(lab)
+        # store_class(lab, res_path)
+        # # test_yolo_label(rect_path, res_path, class_names, resize = False, filtered=False)
+        # filter_nodes_class(res_path, img)
+        # res_im = test_yolo_label(rect_path, res_path, class_names, resize = True, filtered=False)
+        # save_im(res_im, rect_path)
+        # w_pieces, b_pieces = pieces_list(res_path)
+        # print(w_pieces, b_pieces)
+        # # test_yolo_label(rect_path,res_path,class_names, resize=True, filtered = False)
+        # # axes, corner = board(radius, origin, step, width, wstep)
+        # # add_pieces(axes, corner, w_pieces, b_pieces)
 
-    im = get_board(image_path)
-    im = cv2.resize(im, None, fx = 0.4, fy = 0.4)
-    title = f"rectified board"
-    cv2.namedWindow(title)
-    while True:
-        cv2.imshow(title,im)
-        k = cv2.waitKey(30)
-        if k == 113:
-            break
-    cv2.destroyAllWindows()
-    im_path = save_im(im, rect_path)
-    img, lab = infer(model, rect_path)
-    print(lab)
-    store_class(lab, res_path)
-    test_yolo_label(rect_path, res_path, class_names, resize = False, filtered=False)
-    filter_nodes_class(res_path, img)
-    test_yolo_label(rect_path, res_path, class_names, resize = False, filtered=False)
-    # title = f"pieces detection"
-    # cv2.namedWindow(title)
-    # while True:
-    #     cv2.imshow(title,im)
-    #     k = cv2.waitKey(30)
-    #     if k == 113:
-    #         break
-    # cv2.destroyAllWindows()
+
+
+        end=time.time()
+        print('process time :', end-beg)
 
 
     
